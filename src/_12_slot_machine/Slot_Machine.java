2ed5df0edf309fe189b5f8f271c644450491384a
23nfl <23nfl@DESKTOP-DGNSB50.attlocal.net>
2019-09-01 07:58:21 -0700

    SlotMachine

--- src/_12_slot_machine/Slot_Machine.java
+++ src/_12_slot_machine/Slot_Machine.java
@@ -1,5 +1,6 @@
 package _12_slot_machine;
 
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
@@ -14,21 +15,30 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Slot_Machine implements ActionListener {
-	JFrame f= new JFrame();
-	JPanel p= new JPanel();
-	JButton b = new JButton();
 	int r0 = new Random().nextInt(2);
 	int r1 = new Random().nextInt(2);
 	int r2 = new Random().nextInt(2);
+	JButton b = new JButton();
 public static void main(String[] args) {
 	Slot_Machine s = new Slot_Machine();
+	s.randoms();
+    s.createUI();
 }
-void createUI() {
+ void createUI() {
+	JFrame f= new JFrame();
+	JPanel p= new JPanel();
+	p.add(b);
+	f.add(p);
+	f.setVisible(true);
+	f.pack();
 	b.addActionListener(this);
 }
 public void actionPerformed(ActionEvent arg0) {
   if (arg0.getSource()==b) {
-	  
+	  r0=new Random().nextInt(2);
+	  r1=new Random().nextInt(2);
+	  r2=new Random().nextInt(2);
+
   }
 }
 
@@ -45,11 +55,101 @@ return imageLabel;
 void randoms() {
 	if (r0==0) {
 		try {
-			JLabel l=createLabelImage("Cherry.jpeg");
-		} catch (MalformedURLException e) {
+			JLabel cherry=createLabelImage("cherry.jpg");
+		} 
+
+		catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	if (r0==1) {
+		try { 
+			JLabel lemon= createLabelImage("lemon.png");
+		}
+		catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		
+	}
+	}
+		if (r0==2) {
+			try { 
+				JLabel seven= createLabelImage("seven.png");
+			}
+			catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			
+		
 }
 }
+
+if (r1==0) {
+	try {
+		JLabel cherry=createLabelImage("cherry.jpg");
+	} 
+
+	catch (MalformedURLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+if (r1==1) {
+	try { 
+		JLabel lemon= createLabelImage("lemon.png");
+	}
+	catch (MalformedURLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	
+}
+}
+	if (r1==2) {
+		try { 
+			JLabel seven= createLabelImage("seven.png");
+		}
+		catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		
+	
+}
+}
+
+if (r2==0) {
+	try {
+		JLabel cherry=createLabelImage("cherry.jpg");
+	} 
+
+	catch (MalformedURLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+if (r2==1) {
+	try { 
+		JLabel lemon= createLabelImage("lemon.png");
+	}
+	catch (MalformedURLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	
+}
+}
+	if (r2==2) {
+		try { 
+			JLabel seven= createLabelImage("seven.png");
+		}
+		catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		
+	
+}
+}
+}
+}
+
+
+
