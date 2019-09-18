3741789940e8fd62d67602c419ffc178f222287a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-09-18 16:55:21 -0700

    typingTutor

---
+++ src/_05_typing_tutor/typingRunner.java
@@ -0,0 +1,64 @@
+package _05_typing_tutor;
+
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class typingRunner implements KeyListener {
+static JFrame frame = new JFrame();
+static JPanel panel = new JPanel();
+static JLabel label = new JLabel();
+static char currentLetter;
+
+public static void main(String[] args) {
+	typingRunner r = new typingRunner();
+currentLetter = generateRandomLetter1();
+label.setText(currentLetter+"");
+r.idk();
+}
+
+
+static char generateRandomLetter1() {
+      Random r = new Random();
+      return (char) (r.nextInt(26) + 'a');
+}
+ void idk() {
+	frame.add(panel);
+	panel.add(label);
+	frame.addKeyListener(this);
+	label.setFont(label.getFont().deriveFont(300.0f));
+	label.setHorizontalAlignment(JLabel.CENTER);
+	frame.setVisible(true);
+	frame.pack();
+	
+}
+
+
+@Override
+public void keyPressed(KeyEvent arg0) {
+	// TODO Auto-generated method stub
+	System.out.println(arg0);
+	currentLetter = generateRandomLetter1();
+	label.setText(currentLetter+"");
+     if (arg0.getSource()==KeyEvent);
+}
+
+
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+}
